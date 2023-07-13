10e818faae79b83013c79aeb6403c7f298a4bb42
Samuel Castan <samuel.castan96@gmail.com>
2023-07-13 10:48:48 -0600

    finish ingestion.py module

--- ingestion.py
+++ ingestion.py
@@ -1,10 +1,14 @@
+"""
+Looks for new data to ingest
+
+Author: Samuel Castan
+Date: July, 2023
+"""
 import pandas as pd
 import numpy as np
 import os
 import json
 from datetime import datetime
-
-
 
 
 #############Load config.json and get input and output paths
@@ -18,7 +22,41 @@ output_folder_path = config['output_folder_path']
 
 #############Function for data ingestion
 def merge_multiple_dataframe():
-    #check for datasets, compile them together, and write to an output file
+    """
+    Checks for datasets, compile them together, and write to an output file
+    """
+
+    # Set the file extension to search for
+    extension = '.csv'
+
+    # Empty list to store the file names to import briefly
+    file_names = []
+
+    # Loop through all the files in the directory
+    for filename in os.listdir(input_folder_path):
+        # Check if the file matches the desired extension
+        if filename.endswith(extension):
+            # append filename to list for further reading
+            file_names.append(filename)
+
+    # Empty dataframe to store all the files to import
+    df = pd.DataFrame()
+
+    # Concatenating new data into a dataframe
+    for file in file_names:
+        _ = pd.read_csv(os.path.join(input_folder_path, file))
+        df = pd.concat([df, _])
+
+    # Removing duplicated records
+    df = df.drop_duplicates()
+
+    # Ingest dataframe into output folder
+    df.to_csv(os.path.join(output_folder_path, "finaldata.csv"), index=False)
+
+
+    with open(os.path.join(output_folder_path, "ingestedfiles.txt"), "w") as text_file:
+        text_file.write(",".join(file_names))
+
 
 
 
