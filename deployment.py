48a24bae93492dc00137c32d3445990d3b3e6042
Samuel Castan <samuel.castan96@gmail.com>
2023-07-25 16:57:44 -0600

    finishing deployment file and its outputs

--- deployment.py
+++ deployment.py
@@ -1,12 +1,6 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import json
+import shutil
 
 
 
@@ -14,14 +8,18 @@ import json
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'])
+models_output_path = os.path.join(config['output_model_path'])
 prod_deployment_path = os.path.join(config['prod_deployment_path']) 
 
 
 ####################function for deployment
-def store_model_into_pickle(model):
+def store_model_into_pickle():
     #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
 
+    shutil.copy(os.path.join(dataset_csv_path, 'ingestedfiles.txt'), prod_deployment_path)
+    shutil.copy(os.path.join(models_output_path, 'trainedmodel.pkl'),prod_deployment_path)
+    shutil.copy(os.path.join(models_output_path, 'latestscore.txt'),prod_deployment_path)
+
+if __name__ == '__main__':
+    store_model_into_pickle()
