748341b93199ac85bc4afa369e42f0c56be77a4e
Samuel Castan <samuel.castan96@gmail.com>
2024-01-26 11:41:15 -0600

    autopep8 to deployment.py

--- deployment.py
+++ deployment.py
@@ -3,23 +3,36 @@ import json
 import shutil
 
 
-
-##################Load config.json and correct path variable
-with open('config.json','r') as f:
-    config = json.load(f) 
+# Load config.json and correct path variable
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
 dataset_csv_path = os.path.join(config['output_folder_path'])
 models_output_path = os.path.join(config['output_model_path'])
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
+prod_deployment_path = os.path.join(config['prod_deployment_path'])
 
 
-####################function for deployment
+# Function for deployment
 def store_model_into_pickle():
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
+    # copy the latest pickle file, the latestscore.txt value, and the
+    # ingestfiles.txt file into the deployment directory
 
-    shutil.copy(os.path.join(dataset_csv_path, 'ingestedfiles.txt'), prod_deployment_path)
-    shutil.copy(os.path.join(models_output_path, 'trainedmodel.pkl'),prod_deployment_path)
-    shutil.copy(os.path.join(models_output_path, 'latestscore.txt'),prod_deployment_path)
+    shutil.copy(
+        os.path.join(
+            dataset_csv_path,
+            'ingestedfiles.txt'),
+        prod_deployment_path)
+    shutil.copy(
+        os.path.join(
+            models_output_path,
+            'trainedmodel.pkl'),
+        prod_deployment_path)
+    shutil.copy(
+        os.path.join(
+            models_output_path,
+            'latestscore.txt'),
+        prod_deployment_path)
+
 
 if __name__ == '__main__':
     store_model_into_pickle()
