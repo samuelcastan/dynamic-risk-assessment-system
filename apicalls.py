58fcd809573b457edcf085c20026c494e0c16a27
Samuel Castan <samuel.castan96@gmail.com>
2024-02-01 11:44:23 -0600

    progressing in apicalls.py'
    q
    
    ksdljfs
    f

--- apicalls.py
+++ apicalls.py
@@ -1,20 +1,24 @@
 import requests
 
-#Specify a URL that resolves to your workspace
-URL = "http://127.0.0.1/"
+# Specify a URL that resolves to your workspace
+URL = "http://0.0.0.0:8000/"
 
+data = {
+    "data_path", "testdata.csv"
+}
 
+# Call each API endpoint and store the responses
+# response1 = requests.post(url=URL + "prediction", json=data).text
+response2 = requests.get(url=URL + "scoring").text
+response3 = requests.get(url=URL + "summarystats").text
+response4 = requests.get(url=URL + "diagnostics").text
 
-#Call each API endpoint and store the responses
-response1 = #put an API call here
-response2 = #put an API call here
-response3 = #put an API call here
-response4 = #put an API call here
+# combine all API responses
+responses = {
+    # "prediction": response1,
+    "scoring": response2,
+    "summary_stats": response3,
+    "diagnostics": response4
+}
 
-#combine all API responses
-responses = #combine reponses here
-
-#write the responses to your workspace
-
-
-
+print(responses)
