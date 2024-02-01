fd8cf4e22e2adcd57c343aff5afa3a764024698f
Samuel Castan <samuel.castan96@gmail.com>
2024-02-01 17:52:41 -0600

    finishing fullprocess.py

--- fullprocess.py
+++ fullprocess.py
@@ -1,7 +1,7 @@
 from ingestion import merge_multiple_dataframe
 from training import train_model
+from deployment import store_model_into_pickle
 import scoring
-import deployment
 import diagnostics
 import reporting
 import json
@@ -11,6 +11,7 @@ with open("config.json", "r") as f:
     config = json.load(f)
 
 prod_deployment_path = config["prod_deployment_path"]
+output_model_path = config["output_model_path"]
 input_folder_path = config["input_folder_path"]
 
 # Check and read new data
@@ -35,23 +36,26 @@ new_files = [file for file in file_names if file not in ingestedfiles]
 if len(new_files) > 0:
     merge_multiple_dataframe()
     train_model()
-    
 else:
     print("No new data to ingest")
 
 # Checking for model drift
 # check whether the score from the deployed model is different from the
 # score from the model that uses the newest ingested data
+with open(os.path.join(prod_deployment_path, "latestscore.txt"), "r") as file:
+    prod_score = float(file.read())
 
-
+with open(os.path.join(output_model_path, "latestscore.txt"), "r") as file:
+    latest_score = float(file.read())
 
 # Deciding whether to proceed, part 2
-# if you found model drift, you should proceed. otherwise, do end the
-# process here
-
-
+# if you found model drift, you should proceed. otherwise, do end the process here
 # Re-deployment
 # if you found evidence for model drift, re-run the deployment.py script
-
-# Diagnostics and reporting
-# run diagnostics.py and reporting.py for the re-deployed model
+if latest_score > prod_score:
+    store_model_into_pickle()
+    # Diagnostics and reporting
+    os.system("python diagnostcs.py")
+    os.system("reporting.py")
+else:
+    print("No model drift -> No redeployment")
